600a9e3f369259adcbb277760db60cefa4f9bf57
Ji Chao <chaojifudan@outlook.com>
2017-04-25 22:49:55 +0800

    email

--- order-create-service/src/main/java/orders/OrderCreateApplication.java
+++ order-create-service/src/main/java/orders/OrderCreateApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class OrderCreateApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(OrderCreateApplication.class, args);
