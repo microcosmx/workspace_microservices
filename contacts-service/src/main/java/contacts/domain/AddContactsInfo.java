f706b578e8c2c0791dd22d4b19babb4eef19fadc
Ji Chao <chaojifudan@outlook.com>
2017-04-28 00:04:53 +0800

    merge services

---
+++ contacts-service/src/main/java/contacts/domain/AddContactsInfo.java
@@ -0,0 +1,60 @@
+package contacts.domain;
+
+import java.util.UUID;
+
+public class AddContactsInfo {
+
+    private UUID accountId;
+
+    private String name;
+
+    private int documentType;
+
+    private String documentNumber;
+
+    private String phoneNumber;
+
+    public AddContactsInfo(){
+
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(int documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ order-service/src/main/java/order/repository/OrderRepository.java
@@ -0,0 +1,17 @@
+package order.repository;
+
+import order.domain.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, String> {
+
+    Order findById(UUID id);
+
+    @Query("{ 'accountId' : ?0 }")
+    ArrayList<Order> findByAccountId(UUID accountId);
+}
