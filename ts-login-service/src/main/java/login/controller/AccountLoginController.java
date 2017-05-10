1a58c95d1afa30fb09de301e16089fe86ad115bf
Ji Chao <chaojifudan@outlook.com>
2017-05-10 14:31:16 +0800

    modidy interface and page

--- ts-login-service/src/main/java/login/controller/AccountLoginController.java
+++ ts-login-service/src/main/java/login/controller/AccountLoginController.java
@@ -47,4 +47,32 @@ public class AccountLoginController {
         }
     }
 
+    @RequestMapping(path = "/loginWithPara", method = RequestMethod.POST)
+    public LoginResult loginWithPara(@RequestParam String phoneNum,@RequestParam String password){
+        LoginInfo li = new LoginInfo();
+        li.setPhoneNum(phoneNum);
+        li.setPassword(password);
+        LoginResult lr = accountService.login(li);
+        if(lr.getStatus() == false){
+            System.out.println("[AccountLoginService][Login With Para] Login Fail. No token generate.");
+            return lr;
+        }else{
+            //Post token to the sso
+            System.out.println("[AccountLoginService][Login With Para] Password Right. Put token to sso.");
+            restTemplate = new RestTemplate();
+            PutLoginResult tokenResult = restTemplate.getForObject("http://ts-sso-service:12349/loginPutToken/"
+                    + lr.getAccount().getId().toString(),PutLoginResult.class);
+            System.out.println("[AccountLoginService][Login With Para] Post to sso:" + tokenResult.getToken());
+            if(tokenResult.isStatus() == true){
+                lr.setToken(tokenResult.getToken());
+            }else{
+                lr.setToken(null);
+                lr.setStatus(false);
+                lr.setMessage(tokenResult.getMsg());
+                lr.setAccount(null);
+            }
+            return lr;
+        }
+    }
+
 }
