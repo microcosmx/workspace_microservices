7cac584850da745ad62823a00b2dff08d268e964
microcosmx <15110240026@fudan.edu.cn>
2017-05-27 17:04:59 +0800

    xx

--- 1-services/rest-service-6/src/main/java/hello/HelloController.java
+++ 1-services/rest-service-6/src/main/java/hello/HelloController.java
@@ -1,19 +1,12 @@
 package hello;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import hello.Application.ScheduledProducer;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @RestController
 public class HelloController {
@@ -21,12 +14,6 @@ public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     @Autowired
 	private RestTemplate restTemplate;
-    
-    @Autowired
-	private RabbitTemplate rabbitTemplate;
-    
-    @Autowired
-	private ScheduledProducer scheduledProducer;
 
     @RequestMapping("/hello6")
     public Value hello6(@RequestParam(value="cal", defaultValue="50") String cal) {
@@ -34,30 +21,6 @@ public class HelloController {
         double cal2 = Math.abs(Double.valueOf(cal));
         log.info(String.valueOf(cal2));
         
-        
-        String msg = "";
-        
-        log.info("--------mq message send 1----------");
-        msg = "---------current data----------"+cal2+"----------";
-        rabbitTemplate.convertAndSend(Application.queueName, msg);
-        
-        log.info("--------mq message send 2----------");
-        msg = "---------current data 2----------"+(cal2+3)+"----------";
-        rabbitTemplate.convertAndSend(Application.queueName, msg);
-        
-        log.info("--------mq message send 3----------");
-        msg = "---------current data 3----------"+(cal2+6)+"----------";
-        rabbitTemplate.convertAndSend(Application.queueName, msg);
-        
-        //scheduler & timer
-//        log.info("--------mq message send 1----------");
-//        scheduledProducer.sendMessage("---------current data----------"+cal2+"----------");
-//        log.info("--------mq message send 2----------");
-//        scheduledProducer.sendMessage("---------current data 2----------"+(cal2+3)+"----------");
-//        log.info("--------mq message send 3----------");
-//        scheduledProducer.sendMessage("---------current data 3----------"+(cal2+6)+"----------");
-        
-
         Value value5 = restTemplate.getForObject("http://rest-service-5:16005/hello5?cal="+cal, Value.class);
         Value value4 = restTemplate.getForObject("http://rest-service-4:16004/hello4?cal="+cal, Value.class);
         
