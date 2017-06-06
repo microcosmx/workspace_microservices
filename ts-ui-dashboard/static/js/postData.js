f293d6114e0efbac0d0cf833dd8a879c690967dc
saberlilydian <11302010019@fudan.edu.cn>
2017-06-06 16:04:24 +0800

    修改页面，单个html，用ajax和后台交互

--- ts-ui-dashboard/static/js/postData.js
+++ ts-ui-dashboard/static/js/postData.js
@@ -565,3 +565,70 @@ $("#travel_update_button").click(function(){
 //     });
 // });
 
+$("#travel_booking_button").click(function(){
+    var travelQueryInfo = new Object();
+    travelQueryInfo.startingPlace = $("#travel_booking_startingPlace").val();
+    travelQueryInfo.endPlace = $("#travel_booking_terminalPlace").val();
+    travelQueryInfo.departureTime= $("#travel_booking_datee").val();
+    var travelQueryData = JSON.stringify(travelQueryInfo);
+    $.ajax({
+        type: "post",
+        url: "/travel/query",
+        contentType: "application/json",
+        dataType: "json",
+        data:travelQueryData,
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function(result){
+            var obj = result;
+            $("#tickets_booking_list_table").find("tbody").html("");
+            for(var i = 0,l = obj.length ; i < l ; i++){
+                $("#tickets_booking_list_table").find("tbody").append(
+                    "<tr>" +
+                    "<td>" + i + "</td>" +
+                    "<td>" + obj[i]["tripId"]["type"] + obj[i]["tripId"]["number"] + "</td>" +
+                    "<td>" + obj[i]["startingStation"]                             + "</td>" +
+                    "<td>" + obj[i]["terminalStation"]                             + "</td>" +
+                    "<td>" + obj[i]["startingTime"]                                + "</td>" +
+                    "<td>" + obj[i]["endTime"]                                     + "</td>" +
+                    "<td>" + obj[i]["economyClass"]                                + "</td>" +
+                    "<td>" + obj[i]["confortClass"]                                + "</td>" +
+                    "<td>" + "<button class='btn btn-primary ticket_booking_button'>" + "Booking" + "</button>"  + "</td>" +
+                    "</tr>"
+                );
+            }
+            addListenerToBookingTable();
+        }
+    });
+});
+
+
+//For toggle pages
+$("#microservice_page").click(function(){
+    $("#microservices").css('display','block');
+    $("#flow_one").css('display','none');
+    $("#flow_two").css('display','none');
+    $("#flow_three").css('display','none');
+});
+
+$("#flow_one_page").click(function(){
+    $("#microservices").css('display','none');
+    $("#flow_one").css('display','block');
+    $("#flow_two").css('display','none');
+    $("#flow_three").css('display','none');
+});
+
+$("#flow_two_page").click(function(){
+    $("#microservices").css('display','none');
+    $("#flow_one").css('display','none');
+    $("#flow_two").css('display','block');
+    $("#flow_three").css('display','none');
+});
+
+$("#flow_three_page").click(function(){
+    $("#microservices").css('display','none');
+    $("#flow_one").css('display','none');
+    $("#flow_two").css('display','none');
+    $("#flow_three").css('display','block');
+});
