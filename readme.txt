ef9c35357517d613ddb922b8d5737b204a9a4e70
Ji Chao <chaojifudan@outlook.com>
2017-06-07 10:25:50 +0800

    booking tickets

--- readme.txt
+++ readme.txt
@@ -9,3 +9,4 @@ docker-compose down
 
 docker volume rm $(docker volume ls -qf dangling=true)
 docker images|grep none|awk '{print $3 }'|xargs docker rmi
+直达车票分配比例
--- ts-travel-service/src/main/java/travel/domain/TrainType.java
+++ ts-travel-service/src/main/java/travel/domain/TrainType.java
@@ -13,10 +13,10 @@ public class TrainType {
     private String id;      //车型ID，每个车型一个ID，比如某个型号的动车
 
     @Valid
-    private int economyClass;   //普通座的座位数量
+    private int economyClass;   //二等的座位数量
 
     @Valid
-    private int confortClass;   //商务座的座位数量
+    private int confortClass;   //一等的座位数量
 
     public TrainType(){
 
