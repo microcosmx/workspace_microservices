08ccdbcb566637ffd1a0f00abbd26436d87126d4
microcosmx <15110240026@fudan.edu.cn>
2017-06-08 11:10:38 +0800

    xx

---
+++ 1-services/rest-service-4/src/main/java/hello/AsyncTask.java
@@ -0,0 +1,27 @@
+package hello;
+
+import java.util.concurrent.Future;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component  
+public class AsyncTask {  
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());  
+    
+    @Autowired
+	private RestTemplate restTemplate;
+    
+    @Async("mySimpleAsync")
+    public Future<Value> sendAsyncCal(double cal2) throws InterruptedException{
+    	Value value = restTemplate.getForObject("http://rest-service-3:16003/hello3?name=service-4&cal="+cal2, Value.class);
+        return new AsyncResult<>(value);  
+    }
+    
+}  
--- 1-services/rest-service-4/src/main/java/hello/HelloController.java
+++ 1-services/rest-service-4/src/main/java/hello/HelloController.java
@@ -1,5 +1,9 @@
 package hello;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
@@ -16,14 +20,22 @@ public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     @Autowired
 	private RestTemplate restTemplate;
+    
+    @Autowired  
+    private AsyncTask asyncTask;
 
     @RequestMapping("/hello4")
-    public Value hello4(@RequestParam(value="cal", defaultValue="50") String cal) {
+    public Value hello4(@RequestParam(value="cal", defaultValue="50") String cal) throws InterruptedException, ExecutionException, TimeoutException {
 
         double cal2 = (Double.valueOf(cal) + 10)/1.1; 
         log.info(String.valueOf(cal2));
         
-    	Value value = restTemplate.getForObject("http://rest-service-3:16003/hello3?name=service-4&cal="+cal2, Value.class);
+        //async messages
+        Future<Value> task1 = asyncTask.sendAsyncCal(cal2);
+        
+        Value value = task1.get(2000, TimeUnit.MILLISECONDS);// 设定在2000毫秒的时间内完成
+//		task1.cancel(true);
+
 		log.info(value.toString());
 		return value;
     }
