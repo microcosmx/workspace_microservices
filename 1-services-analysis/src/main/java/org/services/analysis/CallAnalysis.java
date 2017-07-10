b15e23da901568eecd82f2e6aa9f7a8e8b45288d
microcosmx <15110240026@fudan.edu.cn>
2017-07-10 10:13:05 +0800

    xx

--- 1-services-analysis/src/main/java/org/services/analysis/CallAnalysis.java
+++ 1-services-analysis/src/main/java/org/services/analysis/CallAnalysis.java
@@ -26,7 +26,7 @@ public class CallAnalysis {
 		HashMap<String, HashMap<String, Object>> traces = new HashMap<String, HashMap<String, Object>>();
 		List<String> pListAll = new ArrayList<String>();
 
-		String traceStr = readFile("./sample/traces1.json");
+		String traceStr = readFile("./sample/traces-error-normal.json");
 		JSONArray tracelist = new JSONArray(traceStr);
 
 		for (int k = 0; k < tracelist.length(); k++) {
