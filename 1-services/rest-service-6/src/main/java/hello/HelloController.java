b0d00c582242736ebfb5cd9a24872cfa0488dbb3
microcosmx <15110240026@fudan.edu.cn>
2017-07-13 12:28:04 +0800

    xx

--- 1-services/rest-service-6/src/main/java/hello/HelloController.java
+++ 1-services/rest-service-6/src/main/java/hello/HelloController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,9 @@ public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     @Autowired
 	private RestTemplate restTemplate;
+    
+    @Autowired  
+    private AsyncTask asyncTask;
 
     @RequestMapping("/hello6")
     public Value hello6(@RequestParam(value="cal", defaultValue="50") String cal)  throws InterruptedException, ExecutionException{
@@ -25,6 +30,37 @@ public class HelloController {
         log.info(String.valueOf(cal2));
         
         Value value = restTemplate.getForObject("http://rest-service-3:16003/hello3?name=service-6&cal="+cal2, Value.class);
+        
+		log.info(value.toString());
+		return value;
+    }
+    
+    
+    
+    @RequestMapping("/cross_timeout")
+    public Value cross_timeout(@RequestParam(value="cal", defaultValue="50") String cal)  throws InterruptedException, ExecutionException{
+
+        double cal2 = Math.abs(Double.valueOf(cal));
+        log.info(String.valueOf(cal2));
+        
+        int access_times = (int) (Math.random() * 12);
+        log.info("--------------------" + access_times);
+        
+        for(int i = 0; i < access_times; i++){
+        	//async messages
+            Future<Value> task1 = asyncTask.sendAsyncCal(cal2);
+        }
+        
+        
+        Value value = null;
+        if(Math.random() < 0.5){
+        	log.info("----------service 5----------");
+        	value = restTemplate.getForObject("http://rest-service-5:16005/hello5", Value.class);
+        }else{
+        	log.info("----------service 4----------");
+        	value = restTemplate.getForObject("http://rest-service-4:16004/hello4", Value.class);
+        }
+        
         
 		log.info(value.toString());
 		return value;
