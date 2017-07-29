02684b03ff9424b686d60bf16b0b079e3e19289d
Ji Chao <chaojifudan@outlook.com>
2017-07-29 23:01:43 +0800

    .

--- ts-rebook-service/src/main/java/rebook/queue/MsgSendingBean.java
+++ ts-rebook-service/src/main/java/rebook/queue/MsgSendingBean.java
@@ -2,9 +2,7 @@ package rebook.queue;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
@@ -23,24 +21,3 @@ public class MsgSendingBean {
 		source.output().send(MessageBuilder.withPayload(val).build());
 	}
 }
-
-
-
-//@Component
-//@EnableBinding(CustomSource.class)
-//public class MsgSendingBean {
-//
-//	@Autowired
-//	private CustomSource customSource;
-//
-//	public void sayHello(String name) {
-//		customSource.output().send(MessageBuilder.withPayload(name).build());
-//	}
-//
-//}
-//
-//interface CustomSource {
-//	String OUTPUT = "customoutput";
-//	@Output(CustomSource.OUTPUT)
-//	MessageChannel output();
-//}
