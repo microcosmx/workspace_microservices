c524e6b7152847be7bf59943a814b66298a87801
Ji Chao <chaojifudan@outlook.com>
2017-08-08 11:28:00 +0800

    process-seq日志 - 尚有错误

--- 1-services-analysis/src/main/java/org/services/analysis/TraceTranslator.java
+++ 1-services-analysis/src/main/java/org/services/analysis/TraceTranslator.java
@@ -30,9 +30,9 @@ public class TraceTranslator {
 
 //        String path = "./sample/trace-error-queue-seq-multi.json";
 //        String path = "./sample/traces-error-cross-timeout-status.json";
-        String path = "./ts-sample/ts-error-normal/error-normal-raw.json";
+//        String path = "./ts-sample/ts-error-normal/error-normal-raw.json";
 //        String path = "./sample/ts-error-normal/ts-error-normal-preserve.json";
- //       String path = "./sample/ts-error-processes-seq/ts-error-processes-seq-cancel-order.json";
+        String path = "./ts-sample/ts-error-processes-seq/error-processes-seq.json";
 //          String path = "./sample/traces-error-external-normal.json";
 //        String path = "./sample/traces-error-report-ui-seq.json";
 
@@ -41,9 +41,9 @@ public class TraceTranslator {
 //        String destPath = "./output/shiviz-error-processes-seq(chance).txt";
 //        String destPath = "./output/shiviz-error-processes-seq-status.txt";
 //        String destPath = "./output/shiviz-traces-error-cross-timeout-status.txt;
-        String destPath = "./ts-output/error-normal/shiviz-ts-error-normal.txt";
+//        String destPath = "./ts-output/error-normal/shiviz-ts-error-normal.txt";
 //        String destPath = "./output/shiviz-ts-error-normal-preserve.txt";
-//        String destPath = "./output/shiviz-ts-error-processes-seq-cancel-order.json.txt";
+        String destPath = "./ts-output/error-processes-seq/shiviz-ts-error-processes-seq.txt";
 //        String destPath = "./output/shiviz-error-cross-timeout-status.txt";
 //          String destPath = "./output/shiviz-error-external-normal.txt";
 //        String destPath = "./output/shiviz-error-report-ui-seq.txt";
