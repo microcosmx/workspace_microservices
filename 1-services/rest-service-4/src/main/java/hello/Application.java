bd7143e2a23abf0e0ea301b74008a5c8dbf812e3
microcosmx <15110240026@fudan.edu.cn>
2017-08-30 11:38:50 +0800

    xx

--- 1-services/rest-service-4/src/main/java/hello/Application.java
+++ 1-services/rest-service-4/src/main/java/hello/Application.java
@@ -1,5 +1,12 @@
 package hello;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -22,12 +29,39 @@ public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
+	// private SessionFactory sessionFactory = null;
+
 	public static void main(String args[]) {
 		SpringApplication.run(Application.class);
 	}
-	
+
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
+	}
+
+	// @Bean
+	// public SessionFactory sessionFactory() {
+	// // A SessionFactory is set up once for an application!
+	// final StandardServiceRegistry registry = new
+	// StandardServiceRegistryBuilder()
+	// .configure() // configures settings from hibernate.cfg.xml
+	// .build();
+	// try {
+	// sessionFactory = new MetadataSources( registry
+	// ).buildMetadata().buildSessionFactory();
+	// }
+	// catch (Exception e) {
+	// // The registry would be destroyed by the SessionFactory, but we had
+	// trouble building the SessionFactory
+	// // so destroy it manually.
+	// StandardServiceRegistryBuilder.destroy( registry );
+	// }
+	// return sessionFactory;
+	// }
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		return Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
 	}
 }
