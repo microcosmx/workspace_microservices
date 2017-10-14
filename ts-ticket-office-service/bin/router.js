2d75440b4c0a668afc37149613a420a153b6b29a
Blanchedidngding <18321716610@163.com>
2017-10-14 10:38:50 +0800

    restore the pom.xml and docker-compose.yml

--- ts-ticket-office-service/bin/router.js
+++ ts-ticket-office-service/bin/router.js
@@ -7,15 +7,14 @@ router.get('/', function(req, res, next) {
     res.send("welcome to ts-ticket-office-service");
 });
 
-router.get('/init', function(req, res, next) {
-    db.initMongo(function(result){
-        res.set({'Content-Type':'text/json','Encodeing':'utf8'});
-        result = JSON.stringify(result);
-        console.log("initResult=" + result);
-        res.end(result);
-    });
-
-});
+// router.get('/init', function(req, res, next) {
+//     db.initMongo(function(result){
+//         res.set({'Content-Type':'text/json','Encodeing':'utf8'});
+//         result = JSON.stringify(result);
+//         console.log("initResult=" + result);
+//         res.end(result);
+//     });
+// });
 
 router.get('/getAll', function(req, res, next) {
     db.getAll(function(result){
