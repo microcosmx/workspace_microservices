6ef550f19bb6cf68c59442b09764a4ea29dfe130
Blanchedidngding <18321716610@163.com>
2017-10-15 20:23:13 +0800

    ts-ticket-office-service后台数据库操作和前端省市区三级联动显示

--- ts-ticket-office-service/app.js
+++ ts-ticket-office-service/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', router);
+app.use('/office', router);
 
 
 // catch 404 and forward to error handler
