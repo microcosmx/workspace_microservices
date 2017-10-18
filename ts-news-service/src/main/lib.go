3373cbd709a54f9fdd025ae57dfd0f90928b69d3
Ji Chao <chaojifudan@outlook.com>
2017-10-18 15:58:32 +0800

    增加news服务抓取网易的网页内容

--- ts-news-service/src/main/lib.go
+++ ts-news-service/src/main/lib.go
@@ -1,5 +1,5 @@
 package main
 
-func sayHello(address string) string {
+func sayHello2(address string) string {
 	return "[Ts-News-Service] Get Message From Client: " + address
 }
--- ts-news-service/src/main/main.go
+++ ts-news-service/src/main/main.go
@@ -2,9 +2,41 @@ package main
 
 import (
 	"github.com/hoisie/web"
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"os"
 )
 
+func hello(val string) string {
+	return fetch()
+}
+
+func fetch() string {
+	urls := []string{"http://www.163.com", "http://www.csdn.net"}
+	var result string
+	result = ""
+	for _, url := range urls {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		result += string(b)
+		fmt.Printf("%s", b)
+	}
+	return result
+}
+
+
+
 func main() {
-	web.Get("/(.*)", sayHello("Test"))
+	web.Get("/(.*)", hello)
 	web.Run("0.0.0.0:12862")
 }
