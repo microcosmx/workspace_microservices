e9a9f3fa349e36e012f5a7f4cbcfaf11a9437306
lab401\lwh <2391289924@qq.com>
2017-10-24 15:44:30 +0800

    voucher service with the order problem

--- ts-ui-dashboard/static/js/voucher.js
+++ ts-ui-dashboard/static/js/voucher.js
@@ -1,10 +1,23 @@
 function onLoadBody(){
     //请求信息封装
     var requestInfo = new Object();
-    requestInfo.orderId = 1;
-    requestInfo.type = 0;
+    requestInfo.orderId = getQueryString('orderId');
+    requestInfo.type = getQueryString('type');
     var data = JSON.stringify(requestInfo);
 
+    /* 获取url后的某一个query的值 */
+    function getQueryString( name ) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg); //获取url中"?"符后的字符串并正则匹配
+        var context = "";
+        if (r != null)
+            context = r[2];
+        reg = null;
+        r = null;
+        return context == null || context == "" || context == "undefined" ? "" : context;
+    }
+
+    //发送请求
     $.ajax({
         type: "post",
         url: "/getVoucher",
--- ts-voucher-service/connect.py
+++ ts-voucher-service/connect.py
@@ -1,12 +1,13 @@
-
+#coding:utf-8
 import pymysql
+
 
 #连接配置信息
 config = {
-    'host':'127.0.0.1',
+    'host':'ts-vouncher-mysql',
     'port':3306,
     'user':'root',
-    'password':'lwh319',
+    'password':'root',
     'db':'voucherservice'
 }
 # 创建连接
