b4fc3335aa885fb4c98e1bc5a1d076c07a33a3e7
Blanchedidngding <18321716610@163.com>
2017-11-06 18:46:15 +0800

    finish the front test of ts-food-service

--- ts-ui-dashboard/static/js/food.js
+++ ts-ui-dashboard/static/js/food.js
@@ -13,7 +13,6 @@ $("#query_food_button").click(function(){
     data.tripId = $('#food_trip_id').val();
 
     // alert(JSON.stringify(data));
-
     $.ajax({
         type: "post",
         url: "/food/getFood",
@@ -24,19 +23,22 @@ $("#query_food_button").click(function(){
             withCredentials: true
         },
         success: function(result){
-            // console.log(result);
+            console.log(result);
 
             var trainFoodList = result.trainFoodList;
-            console.log("trainFoodList:" + trainFoodList);
+            console.log("trainFoodList:" );
+            console.log(trainFoodList[0]);
 
             $("#train_food_list_table").find("tbody").html("");
-            for(var i; trainFoodList && i < trainFoodList.length; i++){
+            for(var i = 0; trainFoodList[0] && i < trainFoodList[0]['foodList'].length; i++){
+                console.log("trainfood"+ i + ":");
+                console.log(trainFoodList[0]['foodList'][i]);
                 $("#train_food_list_table").find("tbody").append(
                     "<tr>" +
-                    "<td>" + i + "</td>" +
-                    "<td>" + trainFoodList["tripId"] + "</td>" +
-                    "<td>" + trainFoodList.foodList[i]['foodName'] + "</td>" +
-                    "<td>" + trainFoodList.foodList[i]['price'] + "</td>" +
+                    "<td>" + (i+1) + "</td>" +
+                    "<td>" + trainFoodList[0]["tripId"] + "</td>" +
+                    "<td>" + trainFoodList[0]['foodList'][i]['foodName'] + "</td>" +
+                    "<td>" + trainFoodList[0]['foodList'][i]['price'] + "</td>" +
                     "</tr>"
                 );
             }
@@ -46,14 +48,14 @@ $("#query_food_button").click(function(){
             for(var key in foodStoreListMap){
                 stationIds.push(key);
                 var fslist = foodStoreListMap[key];
-                showFoodStores(fslist);
+                showFoodStores(key, fslist);
             }
             var stationSelect = document.getElementById ("food_station_select");
             var opt1 = document.createElement ("option");
             opt1.value = 0;
             opt1.innerText = "-- --";
             stationSelect.appendChild(opt1);
-            for(var k=0; k < stationIds.length; k++){
+            for(var k = 0; k < stationIds.length; k++){
                 var opt2 = document.createElement ("option");
                 opt2.value = k + 1;
                 opt2.innerText = stationIds[k];
@@ -69,39 +71,56 @@ $("#query_food_button").click(function(){
 });
 
 function changeFoodStation(){
-    var station = $('#food_station_select').find("option:selected").text()
-    console.log("changeFoodStation");
+    var station = $('#food_station_select').find("option:selected").text();
     var  foodStoreList = foodStoreListMap[station];
-    console.log("foodStoreList" + foodStoreList);
-    showFoodStores(foodStoreList);
+    console.log("foodStoreList:");
+    console.log(foodStoreList);
+    showFoodStores(station, foodStoreList);
 
 }
 
-function showFoodStores(list){
-    console.log("showFoodStores" + list);
+function showFoodStores(station, list){
+    console.log("showFoodStores:");
+    console.log(list);
+    $("#food_of_store").find("tbody").html("");
     $("#food_stores_of_station_list").find("tbody").html("");
     for(var j = 0; j < list.length; j++){
         $("#food_stores_of_station_list").find("tbody").append(
             "<tr>" +
-            "<td>" + j + "</td>" +
+            "<td ><input class='food_index_of_store' type='text' value='" + (j+1) + "'/></td>" +
             "<td>" + list[j]["storeName"] + "</td>" +
             "<td>" + list[j]['telephone'] + "</td>" +
             "<td>" + list[j]['businessTime']  + "</td>" +
             "<td>" + list[j]['deliveryFee']  + "</td>" +
-            "<td>" + "<button class='show_store_foods btn btn-primary' onclick='showFoods(" + list[j]['foodList'] + ")'>Show Foods</button>" + "</td>" +
+            "<td>" + "<button class='show_store_foods btn btn-primary' >Show Foods</button>" + "</td>" +
             "</tr>"
         );
     }
-
+    addListenerToShowFood();
 }
 
-function showFoods(foodList){
-    console.log("foodlist:" + foodList);
+function  addListenerToShowFood(){
+    var showFoodBtn = $(".show_store_foods");
+    for(var i = 0;i < showFoodBtn.length;i++){
+        showFoodBtn[i].onclick = function(){
+            var station = $('#food_station_select').find("option:selected").text();
+            var storeIndex = parseInt($(this).parents("tr").find(".food_index_of_store").val()) - 1;
+            console.log("storeIndex=" + storeIndex);
+            showFoods(station, storeIndex);
+        }
+    }
+}
+
+
+function showFoods(station, j){
+    var foodList = foodStoreListMap[station][j]['foodList'];
+    console.log("foodlist:");
+    console.log(foodList);
     $("#food_of_store").find("tbody").html("");
    for(var j = 0; j < foodList.length; j++){
        $("#food_of_store").find("tbody").append(
            "<tr>" +
-           "<td>" + j + "</td>" +
+           "<td>" + (j+1) + "</td>" +
            "<td>" + foodList[j]["foodName"] + "</td>" +
            "<td>" + foodList[j]['price'] + "</td>" +
            "</tr>"
