d0947d844038a805c7957c3096787de1b098335d
lab401\lwh <2391289924@qq.com>
2017-12-13 15:52:59 +0800

    Add & to the java command. Otherwise the envoy won't start

--- ts-station-service/start_service.sh
+++ ts-station-service/start_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -Xmx200m -jar /app/ts-station-service-1.0.jar
+java -Xmx200m -jar /app/ts-station-service-1.0.jar &
 /usr/local/bin/envoy -c /etc/service-envoy.json --service-cluster station
